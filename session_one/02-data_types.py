538a78fb836035595e40898b931b2eb526ee6f60
Amanda <anikkole796@gmail.com>
2022-09-07 23:01:45 -0700

    added initial code for hello_world and data types

---
+++ session_one/02-data_types.py
@@ -0,0 +1,15 @@
+# Integers - whole numbers
+example_integer = 100
+print(type(example_integer))
+
+# Floats - decimal numbers
+example_float = 1.11
+print(type(example_float))
+
+# Booleans - either True or False
+example_boolean = True
+print(type(example_boolean))
+
+# Strings - string of ASCII characters
+example_string = "I'm a string"
+print(type(example_string))
